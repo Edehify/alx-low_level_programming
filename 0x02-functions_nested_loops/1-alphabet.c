1cded991d1c6b8cb683fd1ebf9cc59072ed05086
Edehify <edehifeanyi16@yahoo.com>
2023-02-22 04:05:47 +0000

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * main - print_alphabet
- * Return: 0
+ * print_alphabet - main function
+ *
+ * Description: print all the letters of the english language
+ * from a to z then a new line. The rule is that the code can
+ * only contain the _putchar function twice
+ * characters contained in _putchar
+ *
+ * Return: always 0
  */
-
 void print_alphabet(void)
 {
-	char c = 'a';
+	char j;
 
-	while (c <= 'z')
+	for (j = 'a'; j <= 'z'; j++)
 	{
-		_putchar(c);
-		c++;
-
+		_putchar(j);
 	}
 	_putchar('\n');
 }
