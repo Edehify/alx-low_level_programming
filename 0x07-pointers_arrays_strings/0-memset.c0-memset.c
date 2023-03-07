5d247bd1c9506eaadacafc883b70d37969efb0af
Edehify <edehifeanyi16@yahoo.com>
2023-03-07 09:15:56 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c0-memset.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _memset - function fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * @s: pointer to the char element
+ * @b: target data
