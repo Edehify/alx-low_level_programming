3cca7c2a1dfb20ba84a18ca52d0ae16fc5fdd1b3
Edehify <edehifeanyi16@yahoo.com>
2023-05-23 18:45:04 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+
+/**
+ * _abs - return the absolute value of a number
+ * @k : the number to be checked
+ * Return: 0 or 1
+ */
+int _abs(int k)
+{
+	if(k < 0)
+	{
+		k = -k ;
+	}
+	return (k);
+}
