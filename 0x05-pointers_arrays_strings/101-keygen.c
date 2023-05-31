bb7392a2673d04771dea5263eb675efbde6fe670
Edehify <edehifeanyi16@yahoo.com>
2023-05-31 03:21:31 +0000

    marcel

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - random password generator
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
-    int sum;
-    char c;
+	int i, sum, n;
+	int pass[100];
 
-    srand(time(NULL));
-    while (sum < 2772 - 122)
-    {
-        c = rand() % 94 + 33;
-        putchar(c);
-        sum += c;
-    }
-    putchar(2772 - sum);
-    return (0);
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
