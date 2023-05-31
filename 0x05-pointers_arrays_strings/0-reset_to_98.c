df13b455ed8987adfea2e2d3df3ba3dbcecca0ed
Edehify <edehifeanyi16@yahoo.com>
2023-05-31 03:27:12 +0000

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - A function that takes a pointer to an int as parameter
  * and updates the value it points to 98.
- * @n - pointer pointing to an interger.
+ * @n: pointer pointing to an interger.
  * @98 - new value of pointer.
  *
  * Return: void
