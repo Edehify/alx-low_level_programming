c003dbb4b9ae6e0255f6992dc45e908acd439872
Edehify <edehifeanyi16@yahoo.com>
2023-06-02 03:02:07 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - convert lower case string to upper
+ * @str: pointer referencing the string array
+ * Return: the resulting string's pointer
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] >= 'a' && s[i])
+		s[i] -= 32;
+		i++;
+	}
+	return (str);
+}
