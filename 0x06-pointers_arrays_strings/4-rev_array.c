7a34c0dc698a83b5ff17df76844d40355780c224
Edehify <edehifeanyi16@yahoo.com>
2023-06-02 02:44:41 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: pointer referencing the integer array
+ * @n: the length of the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp;
+
+	while (i < j)
+	{
+		temo = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+
+		i++;
+		j--;
+	}
+}
