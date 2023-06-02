04e375c635c663a920ea524abc3c81aa6fd2ea6f
Edehify <edehifeanyi16@yahoo.com>
2023-06-02 01:31:41 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = -1;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
@@ -21,6 +21,6 @@ char *_strcat(char *dest, char *src)
 	i++;
 	j++;
 	}
-
+	dest[i] = '\0';
 	return (dest);
 }
