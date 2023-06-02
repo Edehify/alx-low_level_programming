4e78887ee44b2e7502b4a03c81c0da0df44a44c9
Edehify <edehifeanyi16@yahoo.com>
2023-06-02 02:05:30 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings together
+ * @s1: pointer referencing the first string
+ * @s2: pointer referencing the second string
+ * Return: ns1 - ns2 if flase and 0 if true
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	return (0);
+	else
+	return (s1 - s2);
+}
