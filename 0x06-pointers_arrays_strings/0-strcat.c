c1ea9dfc307b2c2d81b0d7dbd3774fa4d43ca74d
Edehify <edehifeanyi16@yahoo.com>
2023-06-02 01:05:25 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - merges or concatenates 2 strings together
+ * @dest: pointer referencing the destination string
+ * @src: pointer referencing source string
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i = -1;
+    int j = 0;
+    while (dest[i] != '\0') {
+        i++;
+    }
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
