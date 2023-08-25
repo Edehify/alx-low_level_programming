331b2a7736685527253720e867dd7aae2e5350a0
edehify <edehifeanyi16@yahoo.com>
2023-08-25 01:24:28 +0000

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void before_main(void);
-
-#pragma data_seg(".CRT$XCU")
-void (*before_main_ptr)(void) = before_main;
-#pragma data_seg()
-
-void before_main(void)
+/**
+ * before_main - Function to print sentence before
+ * running into the main function
+ */
+void __attribute__((constructor)) before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
